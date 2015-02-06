cc51030ad214d9c4e8ea9d5b82ea9deed5338cb6
Mike Lewis <mlewis.mail@gmail.com>
2015-02-06 13:55:39 -0800

    fixup

--- codepath-assignment1/MovieTableViewController.swift
+++ codepath-assignment1/MovieTableViewController.swift
@@ -14,4 +14,22 @@ class MovieTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
         NSLog("Did tap row: \(indexPath.row)")
     }
+
+    func loadData(pathSuffix: String) {
+        if let path = NSBundle.mainBundle().pathForResource("private", ofType: "plist") {
+            if let dict = NSDictionary(contentsOfFile: path) as? Dictionary<String, AnyObject> {
+                let apiKey = dict["RottenTomatoes"]?["key"]
+
+                let fullPath = "http://api.rottentomatoes.com/api/public/v1.0/\(pathSuffix).json?apikey=\(apiKey)"
+                NSLog(fullPath)
+
+                let request = NSMutableURLRequest(URL: NSURL(string: fullPath)!)
+                NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {
+                    (response, data, error) in
+                    var errorValue: NSError? = nil
+                    let dictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &errorValue) as NSDictionary
+                })
+            }
+        }
+    }
 }
